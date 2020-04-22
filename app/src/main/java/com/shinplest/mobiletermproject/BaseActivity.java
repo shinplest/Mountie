e1da6ebe5dfd2d293da1ea42cb38fadb36388a72
shinplest <shinplest@gmail.com>
2020-04-22 13:40:18 +0900

    Added Structure

---
+++ app/src/main/java/com/shinplest/mobiletermproject/BaseActivity.java
@@ -0,0 +1,40 @@
+package com.shinplest.mobiletermproject;
+
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+@SuppressLint("Registered")
+public class BaseActivity extends AppCompatActivity {
+    public ProgressDialog mProgressDialog;
+
+    public void showCustomToast(final String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+}
--- app/src/main/java/com/shinplest/mobiletermproject/main/MainActivity.java
+++ app/src/main/java/com/shinplest/mobiletermproject/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.shinplest.mobiletermproject;
+package com.shinplest.mobiletermproject.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.shinplest.mobiletermproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
