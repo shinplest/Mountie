35bb9f6f0a64bf17a9a41ca4fee6ede10b5fdd30
shinplest <shinplest@gmail.com>
2020-04-29 17:34:00 +0900

    Finish Basic View Structure

--- app/src/main/java/com/shinplest/mobiletermproject/main/MainActivity.java
+++ app/src/main/java/com/shinplest/mobiletermproject/main/MainActivity.java
@@ -2,19 +2,11 @@ package com.shinplest.mobiletermproject.main;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 import com.shinplest.mobiletermproject.BaseActivity;
 import com.shinplest.mobiletermproject.R;
-import com.shinplest.mobiletermproject.dashboard.DashBoardFragment;
-import com.shinplest.mobiletermproject.map.MapFragment;
-import com.shinplest.mobiletermproject.record.RecordFragment;
-
-import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends BaseActivity {
     private ViewPager viewPager;
@@ -23,35 +15,17 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = findViewById(R.id.vp_main);
-        viewPager.setAdapter(new pagerAdapter(getSupportFragmentManager()));
-        viewPager.setCurrentItem(0);
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
+        setViewPager();
     }
 
-    private class pagerAdapter extends FragmentStatePagerAdapter {
-        public pagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @NotNull
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return new RecordFragment();
-                case 1:
-                    return new MapFragment();
-                case 2:
-                    return new DashBoardFragment();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
+    private void setViewPager(){
+        viewPager = findViewById(R.id.vp_main);
+        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+        viewPager.setCurrentItem(1);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setText(getString(R.string.record));
+        tabLayout.getTabAt(1).setText(getString(R.string.map));
+        tabLayout.getTabAt(2).setText(getString(R.string.dashboard));
     }
 }
---
+++ app/src/main/java/com/shinplest/mobiletermproject/main/PagerAdapter.java
@@ -0,0 +1,34 @@
+package com.shinplest.mobiletermproject.main;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.shinplest.mobiletermproject.dashboard.DashBoardFragment;
+import com.shinplest.mobiletermproject.map.MapFragment;
+import com.shinplest.mobiletermproject.record.RecordFragment;
+
+class PagerAdapter extends FragmentStatePagerAdapter {
+    public PagerAdapter(@NonNull FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new RecordFragment();
+            case 1:
+                return new MapFragment();
+            case 2:
+                return new DashBoardFragment();
+        }
+        return new MapFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
