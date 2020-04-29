7dc6e1c8de5dcd923148e5faa3c7cf42ef880295
shinplest <shinplest@gmail.com>
2020-04-29 14:13:38 +0900

    viewpager success

---
+++ app/src/main/java/com/shinplest/mobiletermproject/dashboard/DashBoardFragment.java
@@ -0,0 +1,33 @@
+package com.shinplest.mobiletermproject.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.shinplest.mobiletermproject.R;
+
+
+public class DashBoardFragment extends Fragment {
+    public DashBoardFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dashboard_fragment, container, false);
+        Button RandomMode = view.findViewById(R.id.ramdonmode);
+        return view;
+    }
+}
