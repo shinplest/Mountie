46d378d45f03bb84442999961c9d0587ee270de2
JeongSuKyoung <jsk_1025@naver.com>
2020-05-13 16:06:02 +0900

    for search

--- app/src/main/java/com/shinplest/mobiletermproject/main/MainActivity.java
+++ app/src/main/java/com/shinplest/mobiletermproject/main/MainActivity.java
@@ -3,24 +3,9 @@ package com.shinplest.mobiletermproject.main;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
-import com.google.gson.Gson;
-import com.shinplest.mobiletermproject.ApplicationClass;
 import com.shinplest.mobiletermproject.BaseActivity;
 import com.shinplest.mobiletermproject.NonSwipeableViewPager;
 import com.shinplest.mobiletermproject.R;
-import com.shinplest.mobiletermproject.parsing.Attributes;
-import com.shinplest.mobiletermproject.parsing.Course;
-import com.shinplest.mobiletermproject.parsing.Geometry;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-
-import static com.shinplest.mobiletermproject.ApplicationClass.convertTmToLatLng;
-import static com.shinplest.mobiletermproject.ApplicationClass.testlatlng;
 
 public class MainActivity extends BaseActivity {
 
