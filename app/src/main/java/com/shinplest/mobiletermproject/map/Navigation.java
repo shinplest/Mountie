f1884691cb653c3fa29e08c1ba87720308a42e83
JeonSuHwan <jsuhwan34@gmail.com>
2020-06-06 20:14:37 +0900

    modified

--- app/src/main/java/com/shinplest/mobiletermproject/map/Navigation.java
+++ app/src/main/java/com/shinplest/mobiletermproject/map/Navigation.java
@@ -89,7 +89,7 @@ public class Navigation extends AppCompatActivity implements OnMapReadyCallback 
         record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getBitmapFromView(navigationView);
+                Bitmap bitmap = getBitmapFromView(navigationView);
             }
         });
     }
