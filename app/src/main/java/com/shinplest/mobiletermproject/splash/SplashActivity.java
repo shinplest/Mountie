1db01282d52b7a960bda2bde23da492f2636716f
shinplest <shinplest@gmail.com>
2020-06-25 01:02:56 +0900

    Shared Preference finish

--- app/src/main/java/com/shinplest/mobiletermproject/splash/SplashActivity.java
+++ app/src/main/java/com/shinplest/mobiletermproject/splash/SplashActivity.java
@@ -1,22 +1,28 @@
 package com.shinplest.mobiletermproject.splash;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
 
 import androidx.annotation.Nullable;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.shinplest.mobiletermproject.BaseActivity;
 import com.shinplest.mobiletermproject.R;
 import com.shinplest.mobiletermproject.main.MainActivity;
+import com.shinplest.mobiletermproject.record.RecordItem;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class SplashActivity extends BaseActivity{
+public class SplashActivity extends BaseActivity {
 
     //private Window mWindow;
+    public static ArrayList<RecordItem> recordItems = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +35,15 @@ public class SplashActivity extends BaseActivity{
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences("SharedPreference", MODE_PRIVATE);
+                String json = sharedPreferences.getString("RecordItems", "");
+                if (json == "") {
+                    recordItems = new ArrayList<>();
+                } else {
+                    Gson gson = new GsonBuilder().create();
+                    Type listType = new TypeToken<ArrayList<RecordItem>>() {}.getType();
+                    recordItems = gson.fromJson(json, listType);
+                }
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
