eb91060cd194465e8ca6c63a2a80bded8fb9fecf
shinplest <shinplest@gmail.com>
2020-06-27 03:23:45 +0900

    design

---
+++ app/src/main/java/com/shinplest/mobiletermproject/RecyclerDecoration.java
@@ -0,0 +1,19 @@
+package com.shinplest.mobiletermproject;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerDecoration extends RecyclerView.ItemDecoration {
+    private int space;
+    public RecyclerDecoration(int space) {
+        this.space = space;
+    }
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.bottom = space;
+    }
+}
